5bdac770a61cfce60f3a8117f28b2686e86ea47e
dhanu0510 <dhananjaypatel.official+it@gmail.com>
2024-06-14 14:41:57 -0400

    video upload time

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -6,6 +6,17 @@ const Video = ({ video }) => {
   const createdAt = new Date(video?.createdAt);
   const timeDifference = Math.abs(currentTime - createdAt);
   const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+  const hoursDifference = Math.floor(minutesDifference / 60);
+  const daysDifference = Math.floor(hoursDifference / 24);
+
+  let timeAgo;
+  if (daysDifference > 0) {
+    timeAgo = `${daysDifference} days ago`;
+  } else if (hoursDifference > 0) {
+    timeAgo = `${hoursDifference} hours ago`;
+  } else {
+    timeAgo = `${minutesDifference} minutes ago`;
+  }
 
   return (
     <Grid item lg={3} md={5} sm={6} xs={12}>
@@ -17,9 +28,7 @@ const Video = ({ video }) => {
         />
         <Typography variant="h6">{video?.Title}</Typography>
         <Typography variant="subtitle2">@{video?.User?.username}</Typography>
-        <Typography variant="subtitle2">
-          {minutesDifference} minutes ago
-        </Typography>
+        <Typography variant="subtitle2">{timeAgo}</Typography>
       </Stack>
     </Grid>
   );
