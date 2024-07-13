0c57b2b24bf09e78db8343e73ef7f992b9e9e5f8
aatithya raval <ravalaatithya7@gmail.com>
2024-07-13 01:58:23 -0400

    Video Play: Static URL

---
+++ src/components/video/VideoDescription.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const VideoDescription = () => {
+  return (
+    <Box
+      height="30%"
+      border={1}
+      padding={2}
+      margin={1}
+      borderRadius={5}
+      sx={{
+        overflowY: "auto",
+      }}
+    >
+      <h1>Description</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+        consectetur, turpis nec feugiat blandit, elit libero tincidunt mi, nec
+        luctus nunc turpis sit amet elit. Sed eget libero et nunc ultrices
+        venenatis. Nullam nec nunc nec odio aliquam ultricies. Nulla facilisi.
+        Nullam nec nunc nec odio aliquam ultricies. Nulla facilisi. Nullam nec
+        nunc nec odio aliquam ultricies. Nulla facilisi. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Delectus porro cupiditate impedit
+        inventore exercitationem, necessitatibus eius, laudantium facere labore
+        debitis asperiores eligendi suscipit quod sed excepturi quo molestias,
+        laborum voluptatum. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec consectetur, turpis nec feugiat blandit, elit libero
+        tincidunt mi, nec luctus nunc turpis sit amet elit. Sed eget libero et
+        nunc ultrices venenatis. Nullam nec nunc nec odio aliquam ultricies.
+        Nulla facilisi. Nullam nec nunc nec odio aliquam ultricies. Nulla
+        facilisi. Nullam nec nunc nec odio aliquam ultricies. Nulla facilisi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus porro
+        cupiditate impedit inventore exercitationem, necessitatibus eius,
+        laudantium facere labore debitis asperiores eligendi suscipit quod sed
+        excepturi quo molestias, laborum voluptatum.Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Donec consectetur, turpis nec feugiat
+        blandit, elit libero tincidunt mi, nec luctus nunc turpis sit amet elit.
+        Sed eget libero et nunc ultrices venenatis. Nullam nec nunc nec odio
+        aliquam ultricies. Nulla facilisi. Nullam nec nunc nec odio aliquam
+        ultricies. Nulla facilisi. Nullam nec nunc nec odio aliquam ultricies.
+        Nulla facilisi. Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Delectus porro cupiditate impedit inventore exercitationem,
+        necessitatibus eius, laudantium facere labore debitis asperiores
+        eligendi suscipit quod sed excepturi quo molestias, laborum
+        voluptatum.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec consectetur, turpis nec feugiat blandit, elit libero tincidunt mi,
+        nec luctus nunc turpis sit amet elit. Sed eget libero et nunc ultrices
+        venenatis. Nullam nec nunc nec odio aliquam ultricies. Nulla facilisi.
+        Nullam nec nunc nec odio aliquam ultricies. Nulla facilisi. Nullam nec
+        nunc nec odio aliquam ultricies. Nulla facilisi. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Delectus porro cupiditate impedit
+        inventore exercitationem, necessitatibus eius, laudantium facere labore
+        debitis asperiores eligendi suscipit quod sed excepturi quo molestias,
+        laborum voluptatum.
+      </p>
+    </Box>
+  );
+};
+
+export default VideoDescription;
