9cd2332768c95ec06c1a2230de4731fa68a1ecfd
dhanu0510 <dhananjaypatel.official+it@gmail.com>
2024-08-01 16:26:45 -0400

    DEPLOY

--- src/apis/url.js
+++ src/apis/url.js
@@ -1,3 +1,4 @@
-const URL = "http://localhost:3001";
+//const URL = "http://localhost:3001";
 // const URL = "https://9jeknh6q8j.execute-api.ca-central-1.amazonaws.com";
+const URL = "http://ec2-35-183-44-238.ca-central-1.compute.amazonaws.com:3001";
 export default URL;
